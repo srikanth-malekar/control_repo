b88b7e419e8329da5da3b259fd52ef86eea9049e
srikanth-malekar <92663077+srikanth-malekar@users.noreply.github.com>
2021-10-20 17:19:38 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,8 +3,9 @@ node default {
 node master.puppet.vm {
   include role::master_server
   file { '/root/README':
-  ensure => file
-  content => "Welcome to ${fqdn}"
+    ensure => file
+    content => "Welcome to ${fqdn}"
+  }
 }
 
 node /^web/ {
