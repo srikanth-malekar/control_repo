ca4ea349fd3adffb48d6e0fddc8faaa18717f082
srikanth-malekar <92663077+srikanth-malekar@users.noreply.github.com>
2021-10-21 00:49:15 +0530

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -2,5 +2,13 @@ class profile::agent_nodes {
   include dockeragent
   dockeragent::node { 'web.puppet.vm': }
   dockeragent::node { 'db.puppet.vm': }
+  include host { 'web.puppet.vm':
+    ensure => present,
+    ip => '172.18.0.2',
+  }
+  include host { 'db.puppet.vm':
+    ensure => present,
+    ip => '172.18.0.3',
+  }
   dockeragent::node { 'minetest.puppet.vm': }
 }
